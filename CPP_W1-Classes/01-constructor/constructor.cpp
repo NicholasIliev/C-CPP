a261945ce94c4677a910f1d3c007989153c5b066
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2023-12-04 05:59:29 +0000

    Completion of exercise 1.

--- CPP_W1-Classes/01-constructor/constructor.cpp
+++ CPP_W1-Classes/01-constructor/constructor.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 
 class Pair {
-public:
+private:
     int x;
     int y;
+
+public:
+    // Public constructor to initialize x and y
+    Pair(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    // Public getter functions to access x and y
+    int getX() { 
+        return x; 
+    }
+    int getY() { 
+        return y; 
+    }
 };
 
 int main(int argc, char **argv) {
-    Pair p;
+    // Create an instance of Pair using the constructor
+    Pair p(10, 20);
 
-    p.x = 10;
-    p.y = 20;
-
-    std::cout << "p.x is " << p.x << ", p.y is " << p.y << "\n";
+    std::cout << "p.x is " << p.getX() << ", p.y is " << p.getY() << "\n";
 
     return 0;
 }
