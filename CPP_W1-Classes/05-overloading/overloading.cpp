2b59b21547920ba87fc60f89b78662699f4cfe9d
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2023-12-04 22:11:57 +0000

    Completed exericse 5.

--- CPP_W1-Classes/05-overloading/overloading.cpp
+++ CPP_W1-Classes/05-overloading/overloading.cpp
@@ -25,6 +25,27 @@ int Complex::get_imaginary_part() {
 
 /* implement add_and_print here */
 
+void add_and_print(Complex x, Complex y) {
+    int realTotal = x.get_real_part() + y.get_real_part();
+    int imaginaryTotal = x.get_imaginary_part() + y.get_imaginary_part();
+
+    std::cout << "(" << x.get_real_part() << "+" << x.get_imaginary_part() << "i) " <<  "+ (";
+    std::cout << y.get_real_part() << "+" << y.get_imaginary_part() << "i) " <<  "= (";
+    std::cout << realTotal << "+" << imaginaryTotal << "i)" << std::endl;
+}
+
+// Int + Int
+void add_and_print(int x, int y) {
+    int result = x + y;
+    std::cout << x << " + " << y << " = " << result << std::endl;
+}
+
+// Double + Double
+void add_and_print(double x, double y) {
+    double result = x + y;
+    std::cout << x << " + " << y << " = " << result << std::endl;
+}
+
 int main(int argc, char **argv) {
     Complex c1(4, 5);
     Complex c2(9, 11);
@@ -33,7 +54,7 @@ int main(int argc, char **argv) {
 
     add_and_print(c1, c2); // should print "(4+5i) + (9+11i) = (13+16i)"
     add_and_print(i1, i2); // should print "4 + 9 = 13"
-    add_and_print(d1, d2); // should print "5.8 + 11.2 = 17"
+    add_and_print(d1, d2); // should print "5.8 + 11.std::cout << x.get_imaginary_part() << std::endl;2 = 17"
 
     return 0;
 }
