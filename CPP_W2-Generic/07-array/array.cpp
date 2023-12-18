477181edd2f7fc49063db42942df59a51877ad42
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2023-12-18 06:29:22 +0000

    Compeleted all exercises for Generic.

--- CPP_W2-Generic/07-array/array.cpp
+++ CPP_W2-Generic/07-array/array.cpp
@@ -2,45 +2,45 @@
 #include <cstdlib>
 #include <iostream>
 #include <algorithm> // for min and max
+#include <array>
 
 #define SIZE1 100
 #define SIZE2 154
 
-void add_arrays(
-        int *array1, int *array2, int *result_array,
-        int size1, int size2)
-{
-    int max_size = std::max(size1, size2);
+template <int T1, int T2>
+void add_arrays(std::array<int, T1>& array1, std::array<int, T2>& array2, std::array<int, std::max(T1, T2)>& result_array){
+    int max_size = std::max(T1, T2);
 
     for (int i = 0; i < max_size; ++i) {
-        if (i < size1)
+        if (i < T1)
             result_array[i] = array1[i];
 
-        if (i < size2)
+        if (i < T2)
             result_array[i] += array2[i];
     }
 }
 
-void init_array(int *array, int size) {
-    for (int i = 0; i < size; ++i)
+template <int T>
+void init_array(std::array<int, T>& array) {
+    for (int i = 0; i < T; ++i)
         array[i] = i;
-}
+    }
 
 int main(int argc, char **argv) {
+    constexpr int result_size = std::max(SIZE1, SIZE2);
 
-	int array1[SIZE1];
-	int array2[SIZE2];
+    std::array<int, SIZE1> array1;
+    std::array<int, SIZE2> array2;
 
-    init_array(array1, SIZE1);
-    init_array(array2, SIZE2);
+    init_array<SIZE1>(array1);
+    init_array<SIZE2>(array2);
 
-	int result_size = std::max(SIZE1, SIZE2);
-	int result_array[result_size];
 
-	for (int i = 0; i < result_size; ++i)
-		result_array[i] = 0;
+	std::array<int, result_size> result_array;
 
-    add_arrays(array1, array2, result_array, SIZE1, SIZE2);
+	std::fill(result_array.begin(), result_array.end(), 0);
+
+    add_arrays<SIZE1, SIZE2>(array1, array2, result_array);
 
     for (int i = 0; i < result_size; ++i)
         std::cout << result_array[i] << " ";
